aac9cc5f7a2ee915af91be253bc7ba3edd149b9e
Amishmaqbool <amishmaqbool@gmail.com>
2025-06-05 18:55:15 +0500

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import LayoutWithNavbar from "@/components/layout-with-navbar"
 import Footer from "@/components/footer"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-52PTF9R3');
+          `}
+        </Script>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -41,6 +51,14 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-52PTF9R3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
           <div className="flex flex-col min-h-screen">
             <LayoutWithNavbar>{children}</LayoutWithNavbar>
